92770317c09950ca0c0449806549af6d7939dde4
RulosS290 <146139366+RulosS290@users.noreply.github.com>
2024-09-01 13:07:46 -0500

    Add files via upload

---
+++ src/main/java/co/edu/escuealing/reflexionlab/MicroSpringBoot/App.java
@@ -0,0 +1,13 @@
+package co.edu.escuealing.reflexionlab.MicroSpringBoot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
